564bdb0eeb9cf1b141dd489294d24c52345aea9b
Patricio-Mondragon <42380960+Patricio-Mondragon@users.noreply.github.com>
2018-08-21 20:25:20 -0500

    Programa 5 misión 2
    Patricio Mondragón A01748352 Grupo 2

---
+++ coordenadas.py
@@ -0,0 +1,8 @@
+x1= int(input("Introduce x1:"))
+y1= int(input("Introduce y1:"))
+x2= int(input("Introduce x2:"))
+y2= int(input("Introduce y2:"))
+
+Distancia= ((x2-x1)**2 + (y2-y1)**2)**(1/2)
+
+print("la distancia entre los puntos es:", Distancia)
